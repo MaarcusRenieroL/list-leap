416c8a56bb52054265275bbb157cd71bb6104cf8
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-02-12 12:57:10 +0530

    feat: add server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,21 @@
-const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+import { db } from "@/lib/db";
+
+import { Board } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/board";
+import { Form } from "@/app/(platform)/(dashboard)/organization/[organizationId]/_components/form/form";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} title={board.title} id={board.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
